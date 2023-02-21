4d8eaf2a170c600438d9da2c84d7ae27bb0473fa
Wojciech Mazurek <wmm0003@mix.wvu.edu>
2023-02-21 12:19:03 -0500

    Lab 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeLayoutComponent } from './Layouts/home-layout/home-layout.component';
+import { SecondLayoutComponent } from './Layouts/second-layout/second-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [   
+  {
+  path: '',
+  component: HomeLayoutComponent
+ },
+ {
+  path: 'SecondLayout',
+ component: SecondLayoutComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CardComponent } from './card/card.component';
+import { HomeLayoutComponent } from './Layouts/home-layout/home-layout.component';
+import { SecondLayoutComponent } from './Layouts/second-layout/second-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CardComponent,
+    HomeLayoutComponent,
+    SecondLayoutComponent
   ],
   imports: [
     BrowserModule,
